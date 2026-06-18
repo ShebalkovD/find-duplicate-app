65735f22a34a70666268a65195cdd5328b157257
Denis Shebalkov <shebalkovdenis.wk@gmail.com>
2026-06-18 09:36:54 +0300

    feat: ÑФункÑция переуеворачивания слотов"

--- src/Game/GridCell.tsx
+++ src/Game/GridCell.tsx
@@ -1,26 +1,40 @@
-import {FC} from "react";
-import {StyleSheet, Text} from "react-native";
+import {FC, useState} from "react";
+import {Animated, StyleSheet, Text, TouchableOpacity} from "react-native";
 
 type Props = {
     value: string;
 }
 export const GridCell: FC<Props> = ({value}) => {
+    const [open, setOpen] = useState(true)
+    const opacity = open ? 1 : 0
+
+    const onClick = (): void => {
+        setOpen((prev) => !prev)
+    }
+
     return (
-    <Text style={styles.gridCell}>
-        {value}
-    </Text>
+        <TouchableOpacity style={styles.wrapper} onPressOut={onClick}>
+            <Animated.View style={[{opacity: opacity}]}>
+                <Text style={styles.text}>
+                    {value}
+                </Text>
+            </Animated.View>
+        </TouchableOpacity>
     )
 }
 
 const styles = StyleSheet.create({
-    gridCell: {
+    wrapper: {
         width: '20%',
         aspectRatio: 1,
         backgroundColor: '#2c3031',
         borderRadius: 8,
-        fontSize: 28,
+
         display: 'flex',
         alignItems: "center",
         justifyContent: "center",
     },
+    text: {
+        fontSize: 28,
+    }
 });
