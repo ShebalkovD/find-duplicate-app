ae2237928939885ae93dcc738571ca001a3703cc
Denis Shebalkov <shebalkovdenis.wk@gmail.com>
2026-06-18 14:28:22 +0300

    feat: Роут таблицы очков

--- src/app/score.tsx
+++ src/app/score.tsx
@@ -1,31 +1,70 @@
 import { getScoreList } from "@/Game/utils/getScoreList";
+import { LinkButton } from "@/UI/LinkButton";
 import { FC } from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+
 
 const Score: FC = () => {
     const list = getScoreList();
-    console.log(list)
+
     return (
         <View style={styles.container}>
+            <View style={styles.listContainer}>
+                {list.map((item, index) => (
+                    <View style={styles.line} key={index}>
+                        <Text style={styles.lineText}>{`${item.date} - ${item.score}`}</Text>
+                    </View>
+                ))}
+            </View>
+
+            <LinkButton path={'/'} label={'Назад'} />
 
         </View>
+
     )
 }
 
 const styles = StyleSheet.create({
     container: {
+        height: '100%',
+        width: '100%',
         flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
+        alignItems: 'center',
+        justifyContent: "space-between",
         backgroundColor: '#25292e',
         gap: '1rem',
         color: '#fff',
+        paddingBottom: 24,
+
+    },
+    listContainer: {
+        width: '100%',
+        paddingTop: 24,
+        flex: 1,
+        alignItems: 'flex-start',
+        justifyContent: "flex-start",
+        backgroundColor: '#25292e',
+        gap: '1rem',
+        color: '#fff',
+        height: '70%',
+        overflowY: 'auto',
     },
     text: {
         color: '#fff',
         fontSize: 32,
         fontWeight: 600,
     },
+    line: {
+        paddingHorizontal: 24,
+        paddingVertical: 8,
+        width: '100%',
+        borderBottomWidth: 1,
+        borderBottomColor: 'white',
+    },
+    lineText: {
+        fontSize: 18,
+        color: 'white'
+    }
 });
 
 export default Score;
